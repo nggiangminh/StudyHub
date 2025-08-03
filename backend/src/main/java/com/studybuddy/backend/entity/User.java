a6dc0eb8504025719049b812dc7e159dfc7724e8
Nguyen Giang Minh <nguyengiangminh99@gmail.com>
2025-08-03 22:28:51 +0700

    create entity prototype

---
+++ backend/src/main/java/com/studybuddy/backend/entity/User.java
@@ -0,0 +1,19 @@
+package com.studybuddy.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+
+}
